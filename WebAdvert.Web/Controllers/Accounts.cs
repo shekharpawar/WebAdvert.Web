267bead9e4443d0bf045c4746ddb9be0a07384f5
MSDOMAIN1\SPawar1 <shekhar.pawar@morningstar.com>
2021-12-28 00:23:15 +0530

    Initial Files

---
+++ WebAdvert.Web/Controllers/Accounts.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAdvert.Web.Controllers
+{
+    public class Accounts : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ WebAdvert.Web/Models/Accounts/SignupModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebAdvert.Web.Models.Accounts
+{
+    public class SignupModel
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name ="Email")]
+
+        public string Email { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        [StringLength(6,ErrorMessage ="Password must be 6 char long")]
+        [Display(Name ="Password")]
+
+        public string Password { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        [StringLength(6, ErrorMessage = "Password must be 6 char long")]
+        [Compare("Password",ErrorMessage = "Password and its confirmation do not match")]
+        public string ConfirmPassword { get; set; }
+    }
+}
